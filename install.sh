f6f4b00b8dbf8c1f24a5aacf750ee041469fc242
Edmund Yan <edmund.yan@venmo.com>
2015-11-24 16:50:00 -0500

    Add oh-my-zsh install

--- install.sh
+++ install.sh
@@ -4,3 +4,7 @@ git submodule update --init
 ./brew.sh
 ./_make_symlinks.sh
 ./_osx_defaults.sh
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
